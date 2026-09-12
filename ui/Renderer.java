34c11986ff618e753242710bf94d58b650aa5598
ironfiste1213 <mimouneelgamoussi2@gmail.com>
2026-09-12 22:55:07 +0100

    implement drawing method in Renderer class
    Added drawing functionality to the Renderer class.

--- ui/Renderer.java
+++ ui/Renderer.java
@@ -1,5 +1,5 @@
 package ui;
-
+import javafx.scene.paint.Color;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Renderer {
@@ -8,5 +8,9 @@ public class Renderer {
             GraphicsContext gc,
             simulation.Simulation sim,
             traffic.LightController lights) {
+          gc.clearRect(0, 0, 800, 800);
+          gc.setFill(Color.DARKGRAY);
+          gc.fillRect(0, 360, 800, 80);
+          gc.fillRect(360, 0, 80, 800);
     }
 }
