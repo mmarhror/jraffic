8ce96bb0a9861f91b9ce2a9905e85a1841edb8bb
ironfiste1213 <mimouneelgamoussi2@gmail.com>
2026-09-12 22:32:02 +0100

    implement basic JavaFX application for traffic simulation

---
+++ Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(800, 800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Pane root = new Pane(canvas);
+        Scene scene = new Scene(root, 800, 800);
+        stage.setTitle("Traffic Simulation");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
